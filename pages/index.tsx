43af6d4ac27cfac0f6bb93f568da85302bdf827a
Yosua <godbless_yosua@yahoo.co.id>
2021-06-15 22:10:32 +0700

    change home page to coming soon

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,27 +26,19 @@ export default function Home() {
 						<p>Chrome Dino like game</p>
 					</a>
 
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
+					<a href="/" className={styles.card}>
+						<h2>Coming soon &rarr;</h2>
+						<p>Later</p>
 					</a>
 
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
+					<a href="/" className={styles.card}>
+						<h2>Coming soon &rarr;</h2>
+						<p>Later</p>
 					</a>
 
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
+					<a href="/" className={styles.card}>
+						<h2>Coming soon &rarr;</h2>
+						<p>Later</p>
 					</a>
 				</div>
 			</main>
