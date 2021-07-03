cced7a0bc99645934de8f2fdd0928a0ac9d40a2d
Yosua <godbless_yosua@yahoo.co.id>
2021-07-03 19:23:08 +0700

    Add linting and prettier

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -1,44 +1,48 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import styles from '../../styles/Game.module.css';
-import useSWR from 'swr';
+import styles from '../../styles/Game.module.css'
+import useSWR from 'swr'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function Game() {
-	const router = useRouter();
-	const { id } = router.query;
+  const router = useRouter()
+  const { id } = router.query
 
-	const { data, error } = useSWR(id ? `/api/game-info?id=${id}` : null, id ? fetcher : null);
-	if(error) var content = <div>Failed to load</div>;
-	else if(!data) var content = <div>Loading...</div>;
-	else var content = <iframe className={styles.main} src={data.url} frameBorder={0} />;
+  const { data, error } = useSWR(
+    id ? `/api/game-info?id=${id}` : null,
+    id ? fetcher : null
+  )
+  if (error) var content = <div>Failed to load</div>
+  else if (!data) var content = <div>Loading...</div>
+  else
+    var content = (
+      <iframe className={styles.main} src={data.url} frameBorder={0} />
+    )
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Development Hub</title>
-				<meta name="description" content="Development Hub" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Development Hub</title>
+        <meta name="description" content="Development Hub" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main className={styles.main}>
-				{content}
-			</main>
+      <main className={styles.main}>{content}</main>
 
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
-		</div>
-	)
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,58 +3,56 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Development Hub</title>
-				<meta name="description" content="Development Hub" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Development Hub</title>
+        <meta name="description" content="Development Hub" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to my Dev Hub
-				</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to my Dev Hub</h1>
 
-				<p className={styles.description}>
-					These are the collection of things I developed
-				</p>
+        <p className={styles.description}>
+          These are the collection of things I developed
+        </p>
 
-				<div className={styles.grid}>
-					<a href="/game/dino" className={styles.card}>
-						<h2>Dino Game &rarr;</h2>
-						<p>Chrome Dino like game</p>
-					</a>
+        <div className={styles.grid}>
+          <a href="/game/dino" className={styles.card}>
+            <h2>Dino Game &rarr;</h2>
+            <p>Chrome Dino like game</p>
+          </a>
 
-					<a href="/" className={styles.card}>
-						<h2>Coming soon &rarr;</h2>
-						<p>Later</p>
-					</a>
+          <a href="/" className={styles.card}>
+            <h2>Coming soon &rarr;</h2>
+            <p>Later</p>
+          </a>
 
-					<a href="/" className={styles.card}>
-						<h2>Coming soon &rarr;</h2>
-						<p>Later</p>
-					</a>
+          <a href="/" className={styles.card}>
+            <h2>Coming soon &rarr;</h2>
+            <p>Later</p>
+          </a>
 
-					<a href="/" className={styles.card}>
-						<h2>Coming soon &rarr;</h2>
-						<p>Later</p>
-					</a>
-				</div>
-			</main>
+          <a href="/" className={styles.card}>
+            <h2>Coming soon &rarr;</h2>
+            <p>Later</p>
+          </a>
+        </div>
+      </main>
 
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
-		</div>
-	)
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
 }
